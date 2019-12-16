4ea17fe683d43f44d88d6d23fa90839cb0a0e32c
captnstarburst <conorphinchee@gmail.com>
2019-12-16 17:40:42 +0000

    added svg icon files

--- src/components/UsedAppliances/Sales/Sales.js
+++ src/components/UsedAppliances/Sales/Sales.js
@@ -2,6 +2,15 @@ import React from 'react';
 import {Row, Col} from 'reactstrap';
 // import classes from './Sales.module.css';
 import HeaderText from '../../Typography/HeaderText/HeaderText';
+import { ReactComponent as Icon_Cooktop } from '../../../assets/icons/icon_cooktop.svg';
+import { ReactComponent as Icon_Dishwasher } from '../../../assets/icons/icon_dishwasher.svg';
+import { ReactComponent as Icon_Dryer } from '../../../assets/icons/icon_dryer.svg';
+import { ReactComponent as Icon_Freezer } from '../../../assets/icons/icon_freezer.svg';
+import { ReactComponent as Icon_Fridge } from '../../../assets/icons/icon_fridge.svg';
+import { ReactComponent as Icon_Oven } from '../../../assets/icons/icon_oven.svg';
+import { ReactComponent as Icon_Range } from '../../../assets/icons/icon_range.svg';
+import { ReactComponent as Icon_Washer } from '../../../assets/icons/icon_washer.svg';
+import { ReactComponent as Icon_Water_Heater } from '../../../assets/icons/icon_water_heater.svg';
 
 const Sales = () => (
 		<>
@@ -23,15 +32,17 @@ const Sales = () => (
 	        	</Col>
 	    	</Row>
 	    	<Row className="d-flex justify-content-around">
-	    		<Col md="4">
-	    			<img className="img-fluid rounded"
-	    				src="https://github.com/captnstarburst/thoseguyswebsite/blob/master/public/images/kitchen_appliances.jpeg?raw=true" />
+	    		<Col md="6" className="d-flex justify-content-around">
+	    			<Icon_Cooktop /> 
+	    			<Icon_Dishwasher />
+	    			<Icon_Dryer />
+	    			<Icon_Freezer />
+	    			<Icon_Fridge />
+	    			<Icon_Oven />
+	    			<Icon_Range />
+	    			<Icon_Washer />
+	    			<Icon_Water_Heater />
 	    		</Col>
-	    		<Col md="4">
-	    			<img className="img-fluid rounded"
-	    			src= "https://github.com/captnstarburst/thoseguyswebsite/blob/master/public/images/kitchen_appliances.jpeg?raw=true" />
-	    		</Col>
-	    		
 	    	</Row>
 	    </>
 );
