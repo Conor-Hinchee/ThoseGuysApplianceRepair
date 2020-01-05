aba3cdb1ef488bbff1cf271f12c90a293c98d190
captnstarburst <conorphinchee@gmail.com>
2020-01-05 18:10:14 +0000

    added regex checks to form changes

--- src/components/Home/Body/Contact/MessageModal/ContactInfoForm/ContactInfoForm.js
+++ src/components/Home/Body/Contact/MessageModal/ContactInfoForm/ContactInfoForm.js
@@ -7,6 +7,8 @@ class ContactInfoForm extends Component {
   constructor(props) {
         super(props);
         
+    this.handleFormChange = this.handleFormChange.bind(this);
+    this.checkFormValues = this.checkFormValues.bind(this);
   }
   
   state = {
@@ -17,10 +19,78 @@ class ContactInfoForm extends Component {
 	address2 : "",
 	city : "",
 	zipCode : "",
-	shouldBtnMount: true
+	shouldBtnMount: false
   };
   
   
+  handleFormChange = (e) =>{
+  	const alphaOnly = /^[a-zA-Z]+$/;
+  	const numbersOnly = /^\d+$/;
+  	
+  	
+  	switch(e.target.id){
+  		case "fName":
+  			if(alphaOnly.test(e.target.value) || e.target.value === ""){
+  				this.setState({
+  					fName: e.target.value
+  				});
+  			}
+  			break;
+		case "lName":
+			if(alphaOnly.test(e.target.value) || e.target.value === ""){
+  				this.setState({
+  					lName: e.target.value
+  				});
+  			}
+			break;
+		case "phoneNumber":
+			if(numbersOnly.test(e.target.value) || e.target.value === ""){
+				this.setState({
+					phoneNumber: e.target.value
+				});
+			}
+			break;
+		case "inputAddress":
+			this.setState({
+				inputAddress: e.target.value
+			});
+			break;
+		case "inputAddress2":
+			this.setState({
+				inputAddress2: e.target.value
+			});
+			break;
+		case "inputCity":
+			if(alphaOnly.test(e.target.value) || e.target.value === ""){
+				this.setState({
+					inputCity: e.target.value
+				});
+			}
+			break;
+		case "inputZip":
+			if(numbersOnly.test(e.target.value) || e.target.value === ""){
+				this.setState({
+					inputZip: e.target.value
+				});
+			}
+			break;
+  		default:	
+  			break;
+  	}
+  	
+  	this.checkFormValues();
+  }
+  
+  checkFormValues = () =>{
+  	if(this.state.fName !== "" && this.state.lName !== "" && this.state.phoneNumber !== ""
+  		&& this.state.address !== "" && this.state.address2 !== "" && this.state.city !== ""
+  		&& this.state.zipCode !== ""
+  	){
+  		this.setState({shouldBtnMount: true});
+  	}else{
+  		this.setState({shouldBtnMount: false});
+  	}
+  }
   
   render(){
     return(
@@ -29,41 +99,48 @@ class ContactInfoForm extends Component {
 		      <Row form>
 		        <Col md={6}>
 		          <FormGroup>
-		            <Label for="inputFirstName">First Name</Label>
-		            <Input type="text" name="firstName" id="inputFirstName" placeholder="John" />
+		            <Label for="fName">First Name</Label>
+		            <Input onChange={this.handleFormChange} value={this.state.fName}
+		            	type="text" name="firstName" id="fName" placeholder="John" />
 		          </FormGroup>
 		        </Col>
 		        <Col md={6}>
 		          <FormGroup>
-		            <Label for="inputLastName">Last Name</Label>
-		            <Input type="text" name="LastName" id="inputLastName" placeholder="Smith" />
+		            <Label for="lName">Last Name</Label>
+		            <Input onChange={this.handleFormChange} value={this.state.lName}
+		            	type="text" name="LastName" id="lName" placeholder="Smith" />
 		          </FormGroup>
 		        </Col>
 		      </Row>
 		      <FormGroup>
-		        <Label for="inputPhone">Phone Number</Label>
-		        <Input type="text" name="phoneNumber" id="inputPhone" placeholder="(575)555-5555"/>
+		        <Label for="phoneNumber">Phone Number</Label>
+		        <Input onChange={this.handleFormChange} value={this.state.phoneNumber}
+		        	type="text" name="phoneNumber" id="phoneNumber" placeholder="(575) 555-5555"/>
 		      </FormGroup>
 		      <FormGroup>
 		        <Label for="inputAddress">Address</Label>
-		        <Input type="text" name="address" id="inputAddress" placeholder="1234 Main St"/>
+		        <Input onChange={this.handleFormChange} value={this.state.inputAddress}
+		        	type="text" name="address" id="inputAddress" placeholder="1234 Main St"/>
 		      </FormGroup>
 		      <FormGroup>
 		        <Label for="inputAddress2">Address 2</Label>
-		        <Input type="text" name="address2" id="inputAddress2" placeholder="1234 Main St"/>
+		        <Input onChange={this.handleFormChange} value={this.state.inputAddress2}
+		        	type="text" name="address2" id="inputAddress2" placeholder="1234 Main St"/>
 		      </FormGroup>
 		      <Row form>
 		        <Col md={6}>
 		          <FormGroup>
 		            <Label for="inputCity">City</Label>
-		            <Input type="text" name="city" id="inputCity" placeholder="Clovis" />
+		            <Input onChange={this.handleFormChange} value={this.state.inputCity} 
+		            	type="text" name="city" id="inputCity" placeholder="Clovis" />
 		          </FormGroup>
 		        </Col>
 		        
 		        <Col md={{ size: '2', offset: 4 }}>
 		          <FormGroup>
 		            <Label for="inputZip">Zip</Label>
-		            <Input type="text" name="zip" id="inputZip"/>
+		            <Input onChange={this.handleFormChange} value={this.state.inputZip} 
+		            	type="text" name="zip" id="inputZip"/>
 		          </FormGroup>  
 		        </Col>
 		      </Row>
