89246245db6b6cbb50e5f3b89465359d18234198
captnstarburst <conorphinchee@gmail.com>
2020-01-20 22:32:20 -0500

    added production start script and added paths to server

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 const express = require('express');
+const favicon = require('express-favicon');
+const path = require('path');
+const port = process.env.PORT || 8080;
 const app = express();
+
+
+app.use(favicon(__dirname + '/build/favicon.ico'));
+// the __dirname is the current directory from where the script is running
+app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/send-message', function(req, res){
     const receivedQuery = req.query; 
@@ -50,4 +59,9 @@ app.get('/send-message', function(req, res){
 
 });
 
-app.listen(8080);
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+
+app.listen(port);
