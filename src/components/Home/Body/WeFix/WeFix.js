6df0e3bc3d1d4397f03fe32e60d74596c88434b2
captnstarburst <conorphinchee@gmail.com>
2020-01-21 22:49:24 -0500

    Fixed some sentence structure

--- src/components/Home/Body/WeFix/WeFix.js
+++ src/components/Home/Body/WeFix/WeFix.js
@@ -14,8 +14,8 @@ class WeFix extends Component {
 		brands: [
 			{
 				title : "We Fix It All",
-				text : `Those Guys Appliance Repair can fix any appliance issue, with over 15 years experience there is not 
-						much we have not seen! We fix your Whirlpool appliances, and we fix your Frigidaire appliances, 
+				text : `Those Guys Appliance Repair can fix any appliance issue. With over 15 years experience, there isn't 
+						anything we haven't seen! We fix your Whirlpool appliances, and we fix your Frigidaire appliances, 
 						and we fix your Maytag appliances, and we fix your GE appliances, and we fix your Amana appliances, 
 						and we fix your Samsung appliances, and we fix your Insignia appliances, and we fix your LG appliances,
 						and we fix your Electrolux appliances, and we fix your Bosch appliances, and we fix your Kitchenaid
@@ -26,70 +26,70 @@ class WeFix extends Component {
 				text : `We take care of your Whirlpool appliances!
 						The Whirlpool Corporation is the world's leading manufacturer and marketer of major home appliances. 
 						Whirlpool customers around the world trust Whirlpool to make their lives easier. 
-						We fix broken Whirlpool Refrigerators - broken Whirlpool Ovens - broken Whirlpool Dishwashers - 
-						broken Whirlpool Washers - broken Whirlpool Dryers`,
+						We fix broken Whirlpool refrigerators, broken Whirlpool ovens, broken Whirlpool dishwashers, 
+						broken Whirlpool washers - broken Whirlpool dryers.`,
 				isActive : false					
 			},{
 				title : 'Frigidaire',
 				text : `We take care of your Frigidaire appliances! Frigidaire specializes in offering high-quality,
-						energy efficient kitchen and laundry appliances. We fix broken Frigidaire Ranges - 
-						broken Frigidaire Stoves - broken Frigidaire Refrigerators`,
+						energy efficient kitchen and laundry appliances. We fix broken Frigidaire ranges, 
+						broken Frigidaire stoves, broken Frigidaire refrigerators.`,
 				isActive : false		
 			},{
 				title : 'Maytag',
 				text : `We take care of your Maytag appliances! Maytag offers dependable appliances that are engineered
-						to be durable. We fix broken Maytag Kitchen Appliances - broken Maytag Washers -
-						broken Maytag Dryers`,
+						to be durable. We fix broken Maytag kitchen appliances, broken Maytag washers,
+						broken Maytag dryers.`,
 				isActive : false						
 			},{
 				title : 'GE',
 				text : `We take care of your GE appliances! GE offers a full line of stylish, functional,
-						and high-tech appliances and We fix broken GE Kitchen Appliances - broken GE Washers
-						- broken GE Dryers`,
+						and high-tech appliances and We fix broken GE kitchen appliances, broken GE washers,
+						 broken GE dryers.`,
 				isActive : false		
 			},{
 				title : 'Amana',
 				text : `We take care of your Amana appliances! Amana appliance priced Right. Easy To Use. Quality Products.
-						Amana brand delivers on value. We fix broken Amanda Refrigerators - broken Amanda Freezers - 
-						broken Amanda Dishwashers - broken Amanda Cooktops - broken Amanda Microwaves - 
-						broken Amanda Hoods - broken Amanda Washers`,
+						Amana brand delivers on value. We fix broken Amana refrigerators, broken Amana freezers, 
+						broken Amana dishwashers, broken Amana cooktops, broken Amana microwaves,  
+						broken Amana hoods, broken Amana washers.`,
 				isActive : false							
 			},{
 				title : 'Samsung',
 				text : `We take care of your Samsung appliances! Samsung offers home appliances that create the ideal home
-						you have always dreamed of. We fix broken Samsung Ranges - broken Samsung cooktops - 
-						broken Samsung Ovens - broken Samsung Dishwashers.`,
+						you have always dreamed of. We fix broken Samsung ranges, broken Samsung cooktops,  
+						broken Samsung ovens, broken Samsung dishwashers.`,
 				isActive : false			
 			},{
 				title : 'Insignia',
 				text : `We take care of your Insignia appliances! We fix broken Insignia kitchen appliances - 
-						broken Insignia laundry appliances`,
+						broken Insignia laundry appliances.`,
 				isActive : false			
 			},{
 				title : 'LG',
-				text : `We take care of your LG appliances! LG's Stainless Steel Appliances are highly rated. Reliable. 
-						Energy Star Qualified. State-Of-The-Art. Durable.  We fix broken LG Washers & Dryers - 
-						broken LG Washers - broken LG Washers Refrigerators - broken LG Washers Ranges - broken LG Ovens - 
-						broken LG Dishwashers - broken LG Microwaves`,
+				text : `We take care of your LG appliances! LG's stainless steel appliances are highly rated. Reliable. 
+						Energy star qualified. State-Of-The-Art. Durable. We fix broken LG washers & dryers,  
+						broken LG washers, broken LG refrigerators, broken LG ranges, broken LG ovens, 
+						broken LG dishwashers, broken LG microwaves.`,
 				isActive : false			
 			},{
 				title : 'Bosch',
 				text : `We take care of your Bosch appliances! Bosch has been designing home and kitchen appliances for over 125
 						years, consistently raising the standards in quietness, efficiency and integrated design. 
-						We fix broken Bosch Refrigerators, and Dishwashers`,
+						We fix broken Bosch refrigerators, broken Bosch Dishwashers.`,
 				isActive : false			
 			},{
 				title : 'Electrolux',
 				text : `We take care of your Electrolux appliances! premium kitchen and laundry appliances for your home.  
-						We fix broken Electrolux Refrigerators - broken Electrolux Ranges - broken Electrolux Cooktops - 
-						broken Electrolux Washers - broken Electrolux Dryers`,
+						We fix broken Electrolux refrigerators, broken Electrolux ranges, broken Electrolux cooktops, 
+						broken Electrolux washers, broken Electrolux dryers.`,
 				isActive : false			
 			},{
 				title : 'KitchenAid',
-				text : `We take care of your Kitchenaid appliances! .  We fix broken Kitchenaid Cooktops - 
-						broken Kitchenaid Wall Ovens - broken Kitchenaid Refrigerators - broken Kitchenaid Ranges - 
-						broken Kitchenaid Dishwashers - broken Kitchenaid Disposals - broken Kitchenaid Compactors - 
-						broken Kitchenaid Hoods`,
+				text : `We take care of your Kitchenaid appliances! We fix broken Kitchenaid cooktops,  
+						broken Kitchenaid wall ovens, broken Kitchenaid refrigerators, broken Kitchenaid ranges, 
+						broken Kitchenaid dishwashers, broken Kitchenaid disposals, broken Kitchenaid compactors,  
+						broken Kitchenaid hoods.`,
 				isActive : false			
 			}
 		],
