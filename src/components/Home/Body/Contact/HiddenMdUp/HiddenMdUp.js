f8ab9f7e70e198671746de6052a60d42254e0a9f
captnstarburst <ec2-user@ip-172-31-14-6.us-east-2.compute.internal>
2020-02-02 16:05:06 +0000

    corrected issue with hidden media queries

--- src/components/Home/Body/Contact/HiddenMdUp/HiddenMdUp.js
+++ src/components/Home/Body/Contact/HiddenMdUp/HiddenMdUp.js
@@ -8,13 +8,13 @@ import classes from './HiddenMdUp.module.css';
 
 const HiddenMdUp = (props, {match}) => (
         <Row className={classes.HiddenMdUp}>
-            <Col xs="8" className={classes.SmSpace}>
+            <Col xs="8" className="d-md-none" style={{marginTop:"3%"}}>
                 <Button href="tel:+15757428441" color="secondary" size="lg" block>
                     <FontAwesomeIcon icon={faPhoneSquare} style={{paddingRight: "3px"}}/>
                     Give Us A Call
                 </Button>
             </Col>
-            <Col xs="8" className={classes.SmSpace}>
+            <Col xs="8" className="d-md-none" style={{marginTop:"3%"}}>
                 {/*<Link to={"/messageus/Appliance Picker"} >*/} 
                     <Button color="success" size="lg" block
                         onClick={props.toggle}
@@ -24,7 +24,7 @@ const HiddenMdUp = (props, {match}) => (
                     </Button>
                 {/*</Link>*/}
             </Col>
-            <Col xs="8" className={classes.SmSpace}>
+            <Col xs="8" className="d-md-none" style={{marginTop:"3%"}}>
                 <Button href="https://www.facebook.com/Those-Guys-Appliance-Repair-2000600620203240"
                     rel="noopener noreferrer" color="primary" size="lg" block
                 >
