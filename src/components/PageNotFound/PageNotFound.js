9ddb7e023fad2785596b161655bd9e777b81b093
captnstarburst <ec2-user@ip-172-31-14-6.us-east-2.compute.internal>
2020-02-02 20:33:13 +0000

    404 page completed

---
+++ src/components/PageNotFound/PageNotFound.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Button} from 'reactstrap';
+import Link from  "gatsby-link"
+import classes from './PageNotFound.module.css';
+
+const PageNotFound = () => (
+    <div className={classes.PageNotFound}>
+    	<div className={classes.HeroTextPlacement}>
+    		<h1>404 Error</h1>
+			<h3>Looks like the page is missing or has moved. Sorry about that</h3>
+			    <Link to="/">
+    				<Button outline color="primary" size="lg" style={{color: "white"}}>
+    					Go Back
+    				</Button>
+				</Link>
+    	</div>
+    </div>
+);
+
+export default PageNotFound;
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,14 +1,14 @@
 import React from "react"
-
-import Layout from "../components/layout"
 import SEO from "../components/seo"
+import PageNotFound from "../components/PageNotFound/PageNotFound";
 
 const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+  <>
+    <SEO title="🧦 Lost Socks 🧦" />
+    <main style={{overflow: "hidden"}}>
+      <PageNotFound />
+    </main>
+  </>
 )
 
 export default NotFoundPage
