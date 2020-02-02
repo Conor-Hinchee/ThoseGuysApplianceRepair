e4284287d8ed0bf456c0d475f433e8d411fdc1a1
captnstarburst <ec2-user@ip-172-31-14-6.us-east-2.compute.internal>
2020-02-02 16:41:50 +0000

    added About component

--- src/components/Home/Body/Body.js
+++ src/components/Home/Body/Body.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Container} from 'reactstrap';
 import classes from './Body.module.css';
 import Contact from './Contact/Contact';
-// import About from './About/About';
+import About from './About/About';
 // import Carousel from './Carousel/Carousel';
 // import WeFix from './WeFix/WeFix';
 // import Located from './Located/Located';
@@ -12,6 +12,9 @@ const Body = () => (
     <Container>
     	<section className={classes.Space} id="contact">
     		<Contact  />
+    	</section>
+    	<section className={classes.Space} id="who_we_are">
+    	    <About />
     	</section>
     </Container>
 );
