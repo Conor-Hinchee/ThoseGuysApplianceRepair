f5c008278ab1d75fb041a020b051905e36928263
captnstarburst <ec2-user@ip-172-31-14-6.us-east-2.compute.internal>
2020-02-03 17:45:59 +0000

    added sales component to UsedAppliances

---
+++ src/components/UsedAppliances/Sales/Sales.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import {Row, Col, Button} from 'reactstrap';
+import HeaderText from '../../UI/HeaderText/HeaderText';
+import IconCooktop from "../../../images/icons/icon_cooktop.svg";
+import IconDishwasher from '../../../images/icons/icon_dishwasher.svg';
+import IconDryer from '../../../images/icons/icon_dryer.svg';
+import IconFreezer from '../../../images/icons/icon_freezer.svg';
+import IconFridge from '../../../images/icons/icon_fridge.svg';
+import IconOven from '../../../images/icons/icon_oven.svg';
+import IconRange from '../../../images/icons/icon_range.svg';
+import IconWasher from '../../../images/icons/icon_washer.svg';
+
+const Sales = () => {
+
+	return(
+		<>
+		    <Row className="d-flex justify-content-center">
+		    	<HeaderText 
+		        		tag={1}
+		    	        text={'Appliance Sales'}
+		        />
+		    </Row>
+		    <Row className="d-flex justify-content-center">
+		    	<h5 >Appliances for sale, for less!</h5>
+		    </Row>
+		    <Row>
+	    		<Col className="text-center">
+		        	<p> Those Guys Appliance has used Appliance sets for sale,
+		        		Stoves, Dishwashers for sale and serviced by us! Give us a call or message us today!
+		        	</p>
+	        	</Col>
+	    	</Row>
+	    	<Row className="d-flex justify-content-around">
+	    		<Col md="6" className="d-flex justify-content-around">
+	    			<img src={IconCooktop} alt="cooktop_icon" />
+	    			<img src={IconDishwasher} alt="dishwasher_icon" />
+	    			<img src={IconDryer} alt="dryer_icon" />
+	    			<img src={IconFreezer} alt="freezer_icon"/>
+	    			<img src={IconFridge} alt="fridge_icon" />
+	    			<img src={IconOven} alt="oven_icon" />
+	    			<img src={IconRange} alt="oven_icon" />
+	    			<img src={IconWasher} alt="washer_icon"/>
+	    		</Col>
+	    	</Row>
+	    	<Row className="d-flex justify-content-center" style={{marginTop: "50px"}}>
+				
+					<Button id="Purchase_An_Appliance" size="lg" >
+						Purchase An Appliance
+					</Button>
+				
+			</Row>
+	    </>
+	);	
+};
+
+export default Sales;
--- src/components/UsedAppliances/UsedAppliances.js
+++ src/components/UsedAppliances/UsedAppliances.js
@@ -1,12 +1,13 @@
 import React from "react";
 import {Container} from "reactstrap";
+import Sales from "./Sales/Sales";
 import classes from "./UsedAppliances.module.css";
 
 const UsedAppliances = () =>{
     return(
         <Container>
 			<section className={classes.Space} id="sales">
-					<h1>sjsjslj</h1>
+			    <Sales />
 			</section>
         </Container>      
     );
