680a60bc344c7abcc040492045d1b97a808904c4
captnstarburst <captnstarburst@gmail.com>
2020-02-17 23:02:02 +0000

    message sent success modal

--- src/components/MessageModal/MessageSentModal/MessageSentModal.js
+++ src/components/MessageModal/MessageSentModal/MessageSentModal.js
@@ -1,20 +1,72 @@
-import React, {useState, useEffect} from 'react';
-import {Modal, ModalHeader, ModalBody, Container, Row, Button, Col} from 'reactstrap';
+import React from 'react';
+import {Modal, ModalHeader, ModalBody, Container} from 'reactstrap';
+import { Link } from 'gatsby-plugin-modal-routing';
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import classes from "./MessageSentModal.module.css";
+
 
 const MessageSentModal = (props) => {
 
     const closeBtn = <Link to="/"><button className="close" onClick ={props.clearState}>&times;</button></Link>;
     
+    
+    
+    const mountainTimeZoneVal = new Date().toLocaleString('en-US',{timeZone:'America/Denver'});
+    const mountainDateObj = new Date(mountainTimeZoneVal);
+    const day = mountainDateObj.getDay();
+    const hour = mountainDateObj.getHours();
+    let text = "";
+     
+    if(day>5||day===0||hour<7||hour>19){
+        text = (<h1 className="text-center">We will contact you at our next available office hour thank you for your patience!</h1>);
+    }else{
+        text = (<h1 className="text-center">Thank You ! We will be contacting you with an appointment shortly!</h1>);
+    }
+    
+    
+    
 	return(
 	    <Modal isOpen={true} >
-            <ModalHeader close={closeBtn}>🙌  Message Sent 💖</ModalHeader>
-    		    <ModalBody>
-    			    <Container>
-    				    
-    			    </Container>
-    		    </ModalBody>
-		    </Modal>
+            <ModalHeader close={closeBtn}>
+                <span role="img" aria-label="Raising Hands" aria-hidden="false">🙌</span>  
+                    Message Sent 
+                <span role="img" aria-label="Raising Hands" aria-hidden="false">💖</span>
+            </ModalHeader>
+		    <ModalBody>
+			    <Container>
+			        {text}
+			        <div className={classes.confetti}>
+                          <div className={classes.confettiPiece}></div>
+                          <div className={classes.confettiPiece}></div>
+                          <div className={classes.confettiPiece}></div>
+                          <div className={classes.confettiPiece}></div>
+                          <div className={classes.confettiPiece}></div>
+                          <div className={classes.confettiPiece}></div>
+                          <div className={classes.confettiPiece}></div>
+                          <div className={classes.confettiPiece}></div>
+                          <div className={classes.confettiPiece}></div>
+                          <div className={classes.confettiPiece}></div>
+                          <div className={classes.confettiPiece}></div>
+                          <div className={classes.confettiPiece}></div>
+                          <div className={classes.confettiPiece}></div>
+                    </div>  
+				    
+			    </Container>
+		    </ModalBody>
+		 </Modal>
 	);
 };
 
-export default MessageSentModal;
+MessageSentModal.propTypes = {
+    clearState: PropTypes.func.isRequired
+ };
+  
+
+  const mapDispatchToProps = dispatch => {
+    return { clearState: () => dispatch({type: `CLEAR_STATE`})
+    };
+  };
+  
+
+export default connect(null, mapDispatchToProps)(MessageSentModal);
--- src/pages/message_sent.js
+++ src/pages/message_sent.js
@@ -1,7 +1,6 @@
 import React  from 'react';
 import SEO from "../components/seo";
-import MessageSentModal from "../../components/MessageModal/MessageSentModal/MessageSentModal";
-
+import MessageSentModal from "../components/MessageModal/MessageSentModal/MessageSentModal";
 
 const MessageSent = () => {
     return(
