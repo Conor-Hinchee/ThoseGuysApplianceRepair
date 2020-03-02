71548467a096eb27b310048b2aff217b1f992b77
captnstarburst <captnstarburst@gmail.com>
2020-03-02 23:01:28 +0000

    adding more content to home page

--- src/components/Home/Body/About/About.js
+++ src/components/Home/Body/About/About.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Row, Col} from 'reactstrap';
 import classes from './About.module.css';
 import HeaderText from '../../../UI/HeaderText/HeaderText';
+import Link from  "gatsby-link";
 
 const About = () => (
 	    <Row className={classes.JustifyContentCenter}>
@@ -12,12 +13,31 @@ const About = () => (
 	        <Col md='8' sm='10'>
 	        	<p className={classes.BodyText} style={{marginTop: '5%'}}>
 	        		David Pearson has been in the appliance repair business for over 15 years, and in the spring of 2018, 
-	        		David and his sons started Those Guys Appliance Repair! 
-	        		Proudly serving the Clovis, Portales New Mexico and the surrounding areas!
+	        		David and his sons started Those Guys Appliance Repair!
+	        		We proudly service Clovis, New Mexico and the surrounding areas!
 	        	</p>
 	        	<p className={classes.BodyText}>
-	        		Those Guys Appliance Repair services all brands. If you have a broken appliance in Clovis, NM,
-	        		Those Guys Appliance Repair should be your first call !
+	        		Those Guys Appliance Repair services and repairs all major appliance brands. 
+	        		Take A look 
+	        			<span role="img" aria-label="Eyes" aria-hidden="false"> 👀 </span> 
+	        		at all the 
+	        			<a href="#we_fix"> brands </a> 
+	        		we service!   
+	        		If you have a broken appliance in Clovis, New Mexico or Portales, New Mexico
+	        		Those Guys Appliance Repair should be your first call to get the job done right! 
+	        		<a href="tel:+15757428441"> (575) 742-8441</a>. 
+	        	</p>
+	        	<p className={classes.BodyText}>
+	        		Here at, Those Guys Appliance Repair we do more than just appliance repairs!
+	        		<br/>
+	        		We also provide an Appliance Recycling service; 
+	        		<Link to={'/usedappliances/#recycle'}>turn your old appliance into cash by selling it to us</Link>,
+	        		<br/>
+	        		We sell Used Appliances; 
+	        		<Link to={'/usedappliances/#sales'}>Purchase a used appliance serviced by the professionals</Link>, and
+	        		<br/>
+	        		We do appliance refinishing;
+	        		<Link to={'/usedappliances/#recycle'}>Turn your old appliance into something that looks brand new!</Link>
 	        	</p>
 	        </Col>
 	    </Row>
