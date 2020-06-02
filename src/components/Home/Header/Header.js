102bd0984aa5bd894050daf003c4f19f8dc14287
captnstarburst <conorphinchee@gmail.com>
2020-06-02 09:21:58 -0400

    Removed Header contact row

--- src/components/Home/Header/Header.js
+++ src/components/Home/Header/Header.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import classes from './Header.module.css';
-import ContactRow from './ContactRow/ContactRow';
+// import ContactRow from './ContactRow/ContactRow';
 import Hero from './Hero/Hero';
 
 const Header = () => (
     <header className={classes.Header}>
-        <ContactRow />
+        {/* <ContactRow /> */}
         <Hero />
     </header>
 );
